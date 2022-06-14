3dfbd80b231874cc0b1adda73674432f9d947736
Gustavo <gus.inacio@hotmail.com>
2022-06-14 11:07:59 -0300

    refact: Expenses table with mui

--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -1,54 +1,68 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Button } from '@material-ui/core';
+import { Button,
+  Table, TableBody,
+  TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
 import { deleteExpenses } from '../actions';
 
 function ExpenseTable() {
   const expensesWallet = useSelector((state) => state.wallet.expenses);
   const dispatch = useDispatch();
+
+  const { format } = Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Cambio</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Excluir</th>
-        </tr>
-      </thead>
-      <tbody>
-        {expensesWallet.map((expense) => (
-          <tr key={ expense.id }>
-            <td>{expense.description}</td>
-            <td>{expense.tag}</td>
-            <td>{expense.method}</td>
-            <td>{`${expense.value} ${expense.currency}`}</td>
-            <td>{expense.currency}</td>
-            <td>.</td>
-            <td>
-              {`R$
-               ${expense.value * expense.exchangeRates[expense.currency].ask} `}
-            </td>
-            <td>Real</td>
-            <td>
-              <Button
-                color="secondary"
-                variant="outlined"
-                onClick={ () => dispatch(deleteExpenses(expense.id)) }
-                type="button"
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Descrição</TableCell>
+            <TableCell align="center">Tag</TableCell>
+            <TableCell align="center">Método</TableCell>
+            <TableCell align="center">Valor</TableCell>
+            <TableCell align="center">Moeda</TableCell>
+            <TableCell align="center">Cambio</TableCell>
+            <TableCell align="center">Valor convertido</TableCell>
+            <TableCell align="center">Moeda de conversão</TableCell>
+            <TableCell align="center">Excluir</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {expensesWallet.map((expense) => (
+            <TableRow key={ expense.id }>
+              <TableCell align="center">{expense.description}</TableCell>
+              <TableCell align="center">{expense.tag}</TableCell>
+              <TableCell align="center">{expense.method}</TableCell>
+              <TableCell
+                align="center"
               >
-                Excluir
-              </Button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+                {`${expense.value} ${expense.currency}`}
+
+              </TableCell>
+              <TableCell align="center">{expense.currency}</TableCell>
+              <TableCell align="center">.</TableCell>
+              <TableCell align="center">
+                {format(expense.value * expense.exchangeRates[expense.currency].ask)}
+              </TableCell>
+              <TableCell align="center">Real</TableCell>
+              <TableCell align="center">
+                <Button
+                  color="secondary"
+                  variant="outlined"
+                  onClick={ () => dispatch(deleteExpenses(expense.id)) }
+                  type="button"
+                >
+                  Excluir
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
